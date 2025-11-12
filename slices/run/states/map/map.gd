cee573a49e832b3b20b6de653a42501b86df3487
Lucas McGill <lucasmcgill@lucass-mbp.mynetworksettings.com>
2025-11-11 21:14:12 -0500

    maps can have variable depth and cut off after a limit

--- slices/run/states/map/map.gd
+++ slices/run/states/map/map.gd
@@ -5,17 +5,21 @@ extends Switchable
 @export var RESET_BUTTON := true:
 	set(val):
 		print('DEBUG BUILD')
+		ENCOUNTER_MAP = EncounterRes.new("MATCH", 3, null, 0, 5)
+		CURRENT_ENCOUNTER = ENCOUNTER_MAP
 		ROOT_NODE.clear_children()
-		ROOT_NODE.build()
+		ROOT_NODE.ENCOUNTER = CURRENT_ENCOUNTER
+		ROOT_NODE.build(DEPTH_LIMIT)
 
 @export var ENCOUNTER_MAP : EncounterRes
 @export var CURRENT_ENCOUNTER : EncounterRes
+@export var DEPTH_LIMIT := INF
 
 @onready var ROOT_NODE = %RootNode
 
 func _ready():
-	ENCOUNTER_MAP = EncounterRes.new("MATCH", 3, null, 0, 3)
+	ENCOUNTER_MAP = EncounterRes.new("MATCH", 3, null, 0, 5)
 	CURRENT_ENCOUNTER = ENCOUNTER_MAP
 	ROOT_NODE.ENCOUNTER = CURRENT_ENCOUNTER
-	ROOT_NODE.build()
+	ROOT_NODE.build(DEPTH_LIMIT)
 	print(ENCOUNTER_MAP.to_object())
