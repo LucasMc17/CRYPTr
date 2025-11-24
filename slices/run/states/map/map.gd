8be293aea96f3beb621c4efefcbda6decd3b2fe0
Lucas McGill <lucasmcgill@lucass-mbp.mynetworksettings.com>
2025-11-23 19:08:40 -0500

    does not rebuild map unnecessarily but looking into better solution for initial switchable setups

--- slices/run/states/map/map.gd
+++ slices/run/states/map/map.gd
@@ -4,6 +4,9 @@ extends Switchable
 
 var SHOULD_CREATE_NEW_MAP := true
 
+func setup(init_obj := { "new_map": true }):
+	SHOULD_CREATE_NEW_MAP = init_obj.new_map
+
 func _ready():
 	if SHOULD_CREATE_NEW_MAP:
 		MAP_INSTANCE.init_map(0, 2)
