4a8f4a4ad5577eb2241703e65ce8f72828f3d010
Lucas McGill <lucasmcgill@lucass-mbp.mynetworksettings.com>
2025-12-01 22:53:45 -0500

    even better

--- slices/run/states/map/map.gd
+++ slices/run/states/map/map.gd
@@ -4,15 +4,10 @@ extends Switchable
 @onready var map_instance := %MapInstance
 
 ## Boolean represengint whether the map should generate a new map or recreate the existing on on rendering.
-# var _should_create_new_map := true
-
-func _init():
-	params = { "new_map": true }
-
+var new_map := true
 
 func _ready():
-	if params.new_map:
-	# if _should_create_new_map:
+	if new_map:
 		map_instance.init_map(0, 3)
 	else:
 		map_instance.resume_map()
--- ui_states/switchable.gd
+++ ui_states/switchable.gd
@@ -2,14 +2,11 @@ class_name Switchable
 extends Control
 ## Custom control scene designed to be swapped in and out by a `UISwitcher`. Initializes default parameters for the scene as well via the `setup` method.
 
-## Dictionary representing parameters to setup Switchable with.
-var params := {}
-
 ## Function to setup new Switchable instance with passed parameters. Something like a custom init function for Switchables.
-## Structure of the init_obj param should match the params object. Any keys not found in the params object will have no effect.
+## Structure of the init_obj param sshould match the properties of the Switchable. Any keys not found in the Switchable will have no effect.
 ## Setup function should be used primarily for initializing params, but can be extended to other purposes.
 func setup(init_obj := {}) -> void:
 	for key in init_obj:
 		var value = init_obj[key]
-		if params.has(key):
-			params[key] = value
+		if key in self:
+			self[key] = value
